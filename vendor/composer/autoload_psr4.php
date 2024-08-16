fc149071786774dcb3d180a5e3463e4bd53522e0
spindart <spindart@proton.me>
2024-08-15 22:29:10 -0300

    chore: autoload

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,7 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Budge\\DesignPattern\\' => array($baseDir . '/src'),
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
+    'Budge\\DesignPatterns\\' => array($baseDir . '/src'),
 );
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInit83e94296b12b0fe8c92d962a5e51fd9f
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit83e94296b12b0fe8c92d962a5e51fd9f::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
