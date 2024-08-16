26f371441e9223d209200e66e0af04a5c0831b6e
spindart <spindart@proton.me>
2024-08-15 22:29:47 -0300

    feat: budge

--- src/Budge.php
+++ src/Budge.php
@@ -1,8 +1,19 @@
 <?php
 
-namespace Budge\DesignPattern;
+namespace Budge\DesignPatterns;
 
 class Budge
 {
-    public float $valor;
+    public float $value;
+
+    public function __construct(float $value)
+    {
+        $this->value = $value;
+    }
+
+
+    public function getValue(): float
+    {
+        return $this->value;
+    }
 }
