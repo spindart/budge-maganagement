cb30ae90a7ca964d6aeb3de5b6da40ace12b3023
spindart <spindart@proton.me>
2024-08-17 19:13:24 -0300

    test: discount calculator tests

---
+++ tests/Unit/Behavioral/ChainOfReponsability/DiscountCalculatorTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Invoice\DesignPatterns\Behavioral\Tests\Unit\Behavioral\ChainOfReponsability;
+
+use Invoice\DesignPatterns\Invoice;
+use Invoice\DesignPatterns\Behavioral\ChainOfReponsability\DiscountCalculator;
+use PHPUnit\Framework\TestCase;
+
+class DiscountCalculatorTest extends TestCase
+{
+    private $discountCalculator;
+
+    public function setUp(): void
+    {
+        $this->discountCalculator = new DiscountCalculator();
+    }
+
+    public function testDiscountCalculatorShouldDiscountOverAmount500(): void
+    {
+        //Arrange
+        $invoiceMock = $this->getMockBuilder(Invoice::class)->setConstructorArgs([600, 1])->getMock();
+        $invoiceMock->method('getAmount')->willReturn(600.0);
+
+        //Act
+        $discount = $this->discountCalculator->calculateDiscount($invoiceMock);
+        //Assert
+        $this->assertEquals(30, $discount);
+    }
+
+    public function testDiscountCalculatorShouldDiscountOverFiveItemsAmountOver200(): void
+    {
+        //Arrange
+        $invoiceMock = $this->getMockBuilder(Invoice::class)->setConstructorArgs([201, 6])->getMock();
+        $invoiceMock->method('getAmount')->willReturn(600.0);
+
+        //Act
+        $discount = $this->discountCalculator->calculateDiscount($invoiceMock);
+        //Assert
+        $this->assertEquals(20.1, $discount);
+    }
+
+    public function testDiscountCalculatorShouldNotDiscount(): void
+    {
+        //Arrange
+        $invoiceMock = $this->getMockBuilder(Invoice::class)->setConstructorArgs([200, 6])->getMock();
+        $invoiceMock->method('getAmount')->willReturn(200.0);
+
+        //Act
+        $discount = $this->discountCalculator->calculateDiscount($invoiceMock);
+        //Assert
+        $this->assertEquals(0, $discount);
+    }
+}
