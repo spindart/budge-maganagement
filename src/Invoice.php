adf5970ed35b40ce562a2785daad434da3f7a7bb
spindart <spindart@proton.me>
2024-08-24 19:41:29 -0300

    feat: calculate discount extra without state design pattern behavioral

--- src/Invoice.php
+++ src/Invoice.php
@@ -7,10 +7,29 @@ class Invoice
     public float $amount;
     public int $quantityItems;
 
-    public function __construct(float $amount, $quantityItems)
+    public string $currentStatus;
+
+    public function __construct(float $amount, int $quantityItems)
     {
         $this->amount = $amount;
         $this->quantityItems = $quantityItems ?: 1;
+    }
+
+    public function applyExtraDiscount()
+    {
+        $this->amount -= $this->calculateDiscountExtra();
+    }
+
+    public function calculateDiscountExtra(): float
+    {
+        if ($this->currentStatus == 'IN_APPROVAL') {
+            return $this->amount * 0.05;
+        }
+
+        if ($this->currentStatus == 'APRPROVED') {
+            return $this->amount * 0.02;
+        }
+        throw new \DomainException('Approved and disapproved budgets cannot receive discounts');
     }
 
     public function getAmount(): float
